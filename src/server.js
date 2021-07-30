d14d94b3c3e40a9785b5d906447c68cbeae9a93a
norbertherbert <norbert.herbert@gmail.com>
2021-07-30 15:03:20 +0200

    bugfix

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import express, { json } from 'express';
 
-import cfg from './config.js'; 
+import cfg from './config.js';
 import logger from './logger.js';
 
-import { router as tpxleNITRouter } from './routes/tpxle-nit.router.js';
+import tpxleNITRouter from './routes/tpxle-nit.router.js';
 
 const app = express();
-app.use(json())
+app.use(json());
 app.use('/', tpxleNITRouter);
 
 app.listen(cfg.NIT_SERVER_PORT, () => {
