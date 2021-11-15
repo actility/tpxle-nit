f3fdf92ee3c7fef4f39ccb6241a519777d54b553
Norbert Herbert <norbert.herbert@actility.com>
2021-11-15 19:10:22 +0100

    debug

--- src/models/access-tokens.model.js
+++ src/models/access-tokens.model.js
@@ -7,7 +7,7 @@ export default class AccessTokensModel {
     try {
       const key = `access_token:${clientId}`;
       result = await setAsync(key, accessToken);
-      expireAsync(key, 600); // 10 min
+      expireAsync(key, 300); // 5 min
       logger.debug(`Access Token saved to db. DBKEY: ${key}`);
       // logger.debug(`DBKEY: ${key}:  ${result}`);
     } catch (err) {
