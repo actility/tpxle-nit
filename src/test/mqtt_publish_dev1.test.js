ab182a6b2abd54eea1437e174d20a8c684f30ab5
Norbert Herbert <norbert.herbert@actility.com>
2021-12-14 23:21:26 +0100

    tests updated

--- src/test/mqtt_publish_dev1.test.js
+++ src/test/mqtt_publish_dev1.test.js
@@ -8,8 +8,8 @@ dotenv.config({ path: new URL('./.env', import.meta.url) });
 // const nsName = 'actility'; // NS Specific !!!
 const nsName = 'senet'; // NS Specific !!!
 
-// const leId = 'dev1';
-const leId = 'le-lab';
+const leId = 'dev1';
+// const leId = 'le-lab';
 
 const bodyExampleText = fs.readFileSync(
   new URL(`./uplink_data_sample_from_${nsName}.json`, import.meta.url),
@@ -18,24 +18,24 @@ const bodyExampleText = fs.readFileSync(
 const bodyDev1 = JSON.parse(bodyExampleText);
 
 // bodyDev1.DevEUI_uplink.DevEUI = process.env.DEV_EUI; // NS Specific !!! Actility
-// bodyDev1.devEui = process.env.DEV_EUI.toLowerCase(); // NS Specific !!! Senet
-bodyDev1.devEui = process.env.DEV_EUI_MOBILE_APP.toLowerCase(); // NS Specific !!! Senet
+bodyDev1.devEui = process.env.DEV_EUI.toLowerCase(); // NS Specific !!! Senet
+// bodyDev1.devEui = process.env.DEV_EUI_MOBILE_APP.toLowerCase(); // NS Specific !!! Senet
 
 const url = process.env.BROKER_URL;
 const options = {
   clean: true,
   connectTimeout: 4000,
   // clientId: 'emqx_test',
-  // username: process.env.DEV1_CLIENT_ID,
-  // password: process.env.DEV1_CLIENT_SECRET,
-  username: process.env.LELAB_CLIENT_ID,
-  password: process.env.LELAB_CLIENT_SECRET,
+  username: process.env.DEV1_CLIENT_ID,
+  password: process.env.DEV1_CLIENT_SECRET,
+  // username: process.env.LELAB_CLIENT_ID,
+  // password: process.env.LELAB_CLIENT_SECRET,
   rejectUnauthorized: false,
   // ca: process.env.CA_CERT_LOCATION,
 };
 
-// const topic = `${process.env.CLIENT_ID}/NS/${nsName}/NIT/${process.env.NIT_ID}/LE/${leId}/AS`;
-const topic = `${process.env.CLIENT_ID_KEYCLOAK}/NS/${nsName}/NIT/${process.env.NIT_ID}/LE/${leId}/AS`;
+const topic = `${process.env.CLIENT_ID}/NS/${nsName}/NIT/${process.env.NIT_ID}/LE/${leId}/AS`;
+// const topic = `${process.env.CLIENT_ID_KEYCLOAK}/NS/${nsName}/NIT/${process.env.NIT_ID}/LE/${leId}/AS`;
 console.log(topic);
 
 const mqttClient = mqtt.connect(url, options);
--- src/test/mqtt_subscribe_dev1.test.js
+++ src/test/mqtt_subscribe_dev1.test.js
@@ -8,16 +8,16 @@ const options = {
   clean: true,
   connectTimeout: 4000,
   // clientId: 'emqx_test',
-  // username: process.env.DEV1_CLIENT_ID,
-  // password: process.env.DEV1_CLIENT_SECRET,
-  username: process.env.LELAB_CLIENT_ID,
-  password: process.env.LELAB_CLIENT_SECRET,
+  username: process.env.DEV1_CLIENT_ID,
+  password: process.env.DEV1_CLIENT_SECRET,
+  // username: process.env.LELAB_CLIENT_ID,
+  // password: process.env.LELAB_CLIENT_SECRET,
   rejectUnauthorized: false,
   // ca: process.env.CA_CERT_LOCATION,
 };
 
-// const subscribeTopic = `${process.env.CLIENT_ID}/#`;
-const subscribeTopic = `${process.env.CLIENT_ID_KEYCLOAK}/#`;
+const subscribeTopic = `${process.env.CLIENT_ID}/#`;
+// const subscribeTopic = `${process.env.CLIENT_ID_KEYCLOAK}/#`;
 
 const mqttClient = mqtt.connect(url, options);
 mqttClient.on('connect', () => {
