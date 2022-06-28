9476fc159880dc60c1848f01a2ff5ea908b64766
Norbert Herbert <norbert.herbert@actility.com>
2022-06-28 18:58:44 +0200

    Loriot http connector new files added

---
+++ src/controllers/loriot.controller.js
@@ -0,0 +1,161 @@
+import fetch from 'node-fetch';
+
+import logger from '../logger.js';
+import DownlinkDataModel from '../models/downlink-data.model.js';
+import { translateUplink, translateDownlink } from '../services/nit-loriot.service.js';
+import sendToTPXLEAsync from '../services/send-to-tpxle.js';
+
+export const uplinkFromLoriot = async (req, res, next) => {
+  /* ** Check if request body is correct ** */
+  const errMsg =
+    'The format of "authorization" header must be either "clientId|clientSecret|realm|downlinkUrl" or "|accessToken|realm|downlinkUrl"! "EUI field in body is mandatory!';
+  let accessToken;
+  let clientId;
+  let realm;
+  let clientSecret;
+  let devEUI;
+  let downlinkUrl;
+  try {
+    [clientId, clientSecret, realm, downlinkUrl] = req.headers.authorization.split('|');
+    if (clientId === '') {
+      accessToken = clientSecret;
+    }
+    devEUI = req.body.EUI?.toLowerCase();
+  } catch (err) {
+    logger.warn(err.stack);
+    logger.warn(errMsg);
+    res.status(400).send(errMsg);
+    return;
+  }
+
+  if (!devEUI) {
+    logger.warn('UL: Missing DevEUI!');
+    res.status(400).send('Missing DevEUI!');
+    return;
+  }
+  if (!process.env.NIT__VALID_REALMS.split(',').includes(realm)) {
+    logger.warn('UL: Invalid realm!');
+    res.status(400).send('Invalid realm!');
+    return;
+  }
+  if (!((accessToken || (clientId && clientSecret)) && devEUI && downlinkUrl)) {
+    logger.warn(`UL: DevEUI: ${devEUI}: ${errMsg}`);
+    res.status(400).send(errMsg);
+    return;
+  }
+
+  const nitapikey = req.params.nitapikey || 'loriot'; // For backward compatibility, we allow not specifying nitapikey in the url!
+  /*
+  if (!nitapikey) {
+    logger.warn(`DL: There is no "nitapikey" parameter in the url.`);
+    res.write('There is no "nitapikey" parameter in the url.');
+    res.status(400).end();
+    return;
+  }
+  */
+
+  logger.debug(`UL: DevEUI: ${devEUI}: UL message received from Loriot.`);
+
+  /* ** Save downlink data in db ** */
+  DownlinkDataModel.setDLData(nitapikey, devEUI, { downlinkUrl });
+
+  /* ** Translate message body ** */
+  let translatedBody;
+  try {
+    translatedBody = translateUplink(req.body);
+  } catch (err) {
+    logger.error(err.stack);
+    res.status(400).send('Invalid request body. (Failed to translate request body.)\n');
+    return;
+  }
+
+  // sendToTPXLEAsync(translatedBody, accessToken, clientId, clientSecret, realm);
+  try {
+    await sendToTPXLEAsync(translatedBody, req.tpxleToken, realm, clientId);
+  } catch (err) {
+    next(err);
+    return;
+  }
+
+  res.status(200).end();
+};
+
+export const downlinkToLoriot = async (req, res) => {
+  /* ** Check if request body is correct ** */
+  const devEUI = req.body.deveui?.toLowerCase();
+  if (!devEUI) {
+    logger.warn(`DL: There is no "deveui" field in request body.`);
+    res.write('There is no "deveui" field in request body.');
+    res.status(400).end();
+    return;
+  }
+
+  const nitapikey = req.params.nitapikey || 'loriot'; // For backward compatibility, we allow not specifying nitapikey in the url!
+  /*
+  if (!nitapikey) {
+    logger.warn(`DL: There is no "nitapikey" parameter in the url.`);
+    res.write('There is no "nitapikey" parameter in the url.');
+    res.status(400).end();
+    return;
+  }
+  */
+
+  logger.debug(`DL: DevEUI: ${devEUI}: DL message received from TPXLE.`);
+
+  /* ** Translate message body ** */
+  let translatedBody;
+  try {
+    translatedBody = translateDownlink(req.body);
+  } catch (err) {
+    logger.error(err.stack);
+    res.status(400).send('Invalid request body. (Failed to translate request body.)\n');
+    return;
+  }
+
+  /* ** Get downlinkData from DB. ** */
+  let downlinkData;
+  try {
+    downlinkData = await DownlinkDataModel.getDLData(nitapikey, devEUI);
+  } catch (err) {
+    logger.error(err.stack);
+    res.status(500).end();
+    return;
+  }
+  if (!downlinkData) {
+    logger.warn(`DL: DevEUI: ${devEUI}: DownlinkData does not exists in the db yet.`);
+    res.status(404).send(`DL: DevEUI: ${devEUI}: DownlinkData does not exists in the db yet.\n`);
+    return;
+  }
+
+  /* ** Send downlink frame to NS ** */
+  let nsRes;
+  try {
+    nsRes = await fetch(downlinkData.downlinkUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(translatedBody),
+    });
+  } catch (err) {
+    logger.error(err.stack);
+    res.status(200).end();
+    return;
+  }
+  logger.debug(
+    `DL: DevEUI: ${devEUI}: Downlink forwarded to NS. Response status: ${nsRes.status} ${nsRes.statusText}`,
+  );
+
+  let nsResText;
+  try {
+    nsResText = await nsRes.text();
+  } catch (err) {
+    logger.error(err.stack);
+    return;
+  }
+  if (nsResText) {
+    logger.debug(nsResText);
+  }
+
+  res.status(200).end();
+};
---
+++ src/test/02i_loriot_http_spec.js
@@ -0,0 +1,126 @@
+import fs from 'fs';
+
+const testTarget = process.env.TEST__TARGET; // 'nt' for nano-things, 'lh' for localhost
+const nsName = 'loriot'; // NS Specific !!!
+
+const bodyExampleText = fs.readFileSync(
+  new URL(`./uplink_data_sample_from_${nsName}.json`, import.meta.url),
+);
+
+const bodyDev1 = JSON.parse(bodyExampleText);
+bodyDev1.EUI = process.env.TEST__DEV_EUI; // NS Specific !!! "dev_eui" field
+bodyDev1.downlink_url = process.env.TEST__DL_WEBHOOK; // NS Specific !!! "downlink_url" field
+
+const bodyMobileApp = JSON.parse(bodyExampleText);
+bodyMobileApp.EUI = process.env.TEST__DEV_EUI_MOBILE_APP; // NS Specific !!! "dev_eui" field
+bodyMobileApp.downlink_url = process.env.TEST__DL_WEBHOOK; // NS Specific !!! "downlink_url" field
+
+const bodyRnd = JSON.parse(bodyExampleText);
+bodyRnd.EUI = process.env.TEST__DEV_EUI_RND; // NS Specific !!! "dev_eui" field
+bodyRnd.downlink_url = process.env.TEST__DL_WEBHOOK; // NS Specific !!! "downlink_url" field
+
+const headerCommon = {
+  'content-type': 'application/json',
+};
+
+const headersDev1AccessToken = {
+  Authorization: `|${process.env.TEST__ACCESS_TOKEN}|dev1|${process.env.TEST__DL_WEBHOOK}`,
+  ...headerCommon,
+};
+
+const headersDev1Credentials = {
+  Authorization: `${process.env.TEST__DEV1_TEST__CLIENT_ID}|${process.env.TEST__DEV1_CLIENT_SECRET}|dev1|${process.env.TEST__DL_WEBHOOK}`,
+  ...headerCommon,
+};
+
+const headersKeycloakCredentials = {
+  Authorization: `${process.env.TEST__LELAB_TEST__CLIENT_ID}|${process.env.TEST__LELAB_CLIENT_SECRET}|le-lab|${process.env.TEST__DL_WEBHOOK}`,
+  ...headerCommon,
+};
+
+const headersRndCredentials = {
+  Authorization: `${process.env.TEST__DEV1_TEST__CLIENT_ID}|${process.env.TEST__DEV1_CLIENT_SECRET}|rnd|${process.env.TEST__DL_WEBHOOK}`,
+  ...headerCommon,
+};
+
+const method = 'POST';
+
+const dlBodyDev1 = {
+  type: 'downlink',
+  deveui: process.env.TEST__DEV_EUI,
+  port: '2',
+  payload: '020402',
+};
+
+const dlBodyMobileApp = {
+  type: 'downlink',
+  deveui: process.env.TEST__DEV_EUI_MOBILE_APP,
+  port: '2',
+  payload: '020402',
+};
+
+const dlBodyRnd = {
+  type: 'downlink',
+  deveui: process.env.TEST__DEV_EUI_RND,
+  port: '2',
+  payload: '020402',
+};
+
+const dlHeaders = {
+  'content-type': 'application/json',
+};
+
+const urls = {
+  lh: {
+    ul: `http://localhost:${process.env.NIT__SERVER_PORT}/uplink_from_${nsName}/${process.env.TEST__NITAPIKEY}`,
+    dl: `http://localhost:${process.env.NIT__SERVER_PORT}/downlink_to_${nsName}/${process.env.TEST__NITAPIKEY}`,
+  },
+  nt: {
+    ul: `https://nano-things.net/tpxle-nit/uplink_from_${nsName}/${process.env.TEST__NITAPIKEY}`,
+    dl: `https://nano-things.net/tpxle-nit/downlink_to_${nsName}/${process.env.TEST__NITAPIKEY}`,
+  },
+};
+
+const examples = [
+  // Dev1 - Access Token
+  {
+    url: urls[testTarget].ul,
+    options: { method, headers: headersDev1AccessToken, body: JSON.stringify(bodyDev1) },
+  },
+  {
+    url: urls[testTarget].dl,
+    options: { method, headers: dlHeaders, body: JSON.stringify(dlBodyDev1) },
+  },
+
+  // Dev1 - Credentials
+  {
+    url: urls[testTarget].ul,
+    options: { method, headers: headersDev1Credentials, body: JSON.stringify(bodyDev1) },
+  },
+  {
+    url: urls[testTarget].dl,
+    options: { method, headers: dlHeaders, body: JSON.stringify(dlBodyDev1) },
+  },
+
+  // Mobile App
+  {
+    url: urls[testTarget].ul,
+    options: { method, headers: headersKeycloakCredentials, body: JSON.stringify(bodyMobileApp) },
+  },
+  {
+    url: urls[testTarget].dl,
+    options: { method, headers: dlHeaders, body: JSON.stringify(dlBodyMobileApp) },
+  },
+
+  // RnD
+  {
+    url: urls[testTarget].ul,
+    options: { method, headers: headersRndCredentials, body: JSON.stringify(bodyRnd) },
+  },
+  {
+    url: urls[testTarget].dl,
+    options: { method, headers: dlHeaders, body: JSON.stringify(dlBodyRnd) },
+  },
+];
+
+export default examples;
